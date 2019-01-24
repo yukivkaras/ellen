47d203c39029e41d1e3cf1b0a6cc89b145031539
yukivkaras <497321931@qq.com>
2019-01-24 15:38:14 +0800

    test

---
+++ src/ellen.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+// import App from './App'
+class Ellen extends Component{
+    render(){
+        return (
+            <div>
+                {/* <App/> */}
+                <p>ellen is beautiful</p>  
+                <h1>Yes</h1>
+          </div>
+        )
+    }
+}
+
+export default  Ellen;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+// import Ellen from './ellen'
+import FirstPage from './myTest/test';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<FirstPage />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
